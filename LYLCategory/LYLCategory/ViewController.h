6fb00c422853ad1be8cedfa48f4d30567177c4bb
李银龙 <liyinlong@liyinlongdeimac.local>
2016-11-30 15:58:36 +0800

    类别

---
+++ LYLCategory/LYLCategory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LYLCategory
+//
+//  Created by 李银龙 on 16/11/30.
+//  Copyright © 2016年 李银龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LYLCategory/LYLCategory/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LYLCategory
+//
+//  Created by 李银龙 on 16/11/30.
+//  Copyright © 2016年 李银龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
